fd855a2b2abe3b558162a930b13fd4d354cf5876
Shubzz-z <smore2630@gmail.com>
2024-02-29 20:44:16 +0530

    Spinner Css Added

--- src/Components/Spinner.js
+++ src/Components/Spinner.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import './Spinner.css'
 function Spinner() {
   return (
-    <div className='w-full h-full flex flex-col flex-grow justify-center items-center'>
-        <div className=" spinner">hii</div>
+    <div className='w-full h-full flex flex-col flex-grow justify-center items-center gap-8'>
+        <div className="spinner"></div>
         <h2 className='text-white text-3xl'>Loading Data</h2>
     </div>
   )
